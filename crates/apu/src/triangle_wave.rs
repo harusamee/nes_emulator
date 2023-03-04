535cc0c9ef5443f67563ca7d4a91fbab36b4fe25
Hajime Wakabayashi <hajime.wakabayashi@gmail.com>
2023-03-04 14:00:45 +0900

    Update code

--- crates/apu/src/triangle_wave.rs
+++ crates/apu/src/triangle_wave.rs
@@ -1,13 +1,16 @@
-use crate::wave_trait::WaveTrait;
-use crate::wave_trait::AsWave;
 use crate::wave::Wave;
-
+use crate::wave_trait::AsWave;
+use crate::wave_trait::WaveTrait;
 
 pub struct TriangleWave {
     base: Wave,
-    phase: f32,
     counter_halt: bool,
     linear_counter: u8,
+    //
+    current_output: u8,
+    current_level_bit: u8,
+    freq_counter: u16,
+    linear_counter_internal: u8,
 }
 
 mod private {
@@ -24,24 +27,14 @@ mod private {
 
 impl WaveTrait for TriangleWave {
     fn get_output(&mut self) -> u8 {
-        const WAVEFORM: [u8; 32] = [
-            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, //
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        ];
-
-        let mut result = 0u8;
-        if self.as_wave().enable && self.as_wave().length_counter > 0 && self.linear_counter > 0 {
-            let phase_x32 = (self.phase * 32.0).floor() as usize;
-            result = WAVEFORM[phase_x32];
-        }
-        self.phase = (self.phase + self.as_wave().phase_inc) % 1.0;
-        result
+        self.current_output
     }
 
     fn on_length_count(&mut self) {
         if !self.counter_halt && self.as_wave().length_counter > 0 {
             self.as_mut_wave().length_counter -= 1;
         }
+        self.update_current_output_with_check();
     }
 
     fn on_frame(&mut self) {
@@ -49,6 +42,17 @@ impl WaveTrait for TriangleWave {
     }
 
     fn tick(&mut self, tick: usize) {
+        self.freq_counter -= 1;
+        if self.freq_counter == 0 {
+            self.freq_counter = self.get_freq_11bit();
+            if self.current_level_bit == 31 {
+                self.current_level_bit = 0;
+            } else {
+                self.current_level_bit += 1;
+            }
+
+            self.update_current_output_with_check();
+        }
     }
 }
 
@@ -56,27 +60,50 @@ impl TriangleWave {
     pub fn new() -> Self {
         TriangleWave {
             base: Wave::new("Triangle"),
-            phase: 0.0,
             linear_counter: 0,
             counter_halt: false,
+            current_output: 0,
+            current_level_bit: 0,
+            freq_counter: 0,
+            linear_counter_internal: 0,
         }
     }
 
-    pub fn set_linear_counter(&mut self, linear_counter: u8) {
-        self.linear_counter = linear_counter;
-        // println!(
-        //     "triangle: linear counter {} sec",
-        //     self.linear_counter as f32 / 240.0
-        // );
+    fn update_current_output_with_check(&mut self) {
+        if self.can_output() {
+            self.update_current_output();
+          } else {
+            self.current_output = 0;
+        }
+
+    }
+
+    fn can_output(&self) -> bool {
+        if self.base.enable && self.base.length_counter > 0 && self.linear_counter_internal > 0 {
+            let freq = self.get_freq_11bit();
+            freq >= 0b100
+        } else {
+            false
+        }
+    }
+
+    fn update_current_output(&mut self) {
+        static WAVEFORM: [u8; 32] = [
+            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, //
+            1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        ];
+
+        self.current_output = WAVEFORM[self.current_level_bit as usize];
     }
 
     pub fn on_linear_count(&mut self) {
-        if !self.counter_halt && self.linear_counter > 0 {
-            self.linear_counter -= 1;
-            if self.linear_counter == 0 {
+        if !self.counter_halt && self.linear_counter_internal > 0 {
+            self.linear_counter_internal -= 1;
+            if self.linear_counter_internal == 0 {
                 // println!("triangle: linear count zero");
             }
         }
+        self.update_current_output_with_check();
     }
 
     pub fn set_halt(&mut self, halt: bool) {
@@ -84,5 +111,21 @@ impl TriangleWave {
         if self.counter_halt {
             // println!("triangle: counter halted");
         }
-}
+    }
+
+    pub fn write_0(&mut self, data: u8) {
+        let linear_counter = data & 0b0111_1111;
+        self.linear_counter = linear_counter;
+        self.linear_counter_internal = linear_counter;
+        let halt = data & 0b1000_0000 > 0;
+        self.set_halt(halt);
+    }
+
+    pub fn write_3(&mut self, data: u8) {
+        self.set_reg_freq_hi(data);
+        self.set_length_counter(data);
+        self.set_tone_freq();
+        self.linear_counter_internal = self.linear_counter;
+    }
+
 }
